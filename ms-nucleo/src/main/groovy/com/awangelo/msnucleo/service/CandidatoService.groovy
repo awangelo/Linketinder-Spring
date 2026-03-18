d25961bffc413a0d5906f2ca23b5ba4b2358de40
awangelo <anggg.gaio@gmail.com>
2026-03-18 10:08:08 -0300

    feat: implement service for Candidato creating using passwordEncoder

---
+++ ms-nucleo/src/main/groovy/com/awangelo/msnucleo/service/CandidatoService.groovy
@@ -0,0 +1,46 @@
+package com.awangelo.msnucleo.service
+
+import com.awangelo.msnucleo.dto.CandidatoRequestDTO
+import com.awangelo.msnucleo.dto.CandidatoResponseDTO
+import com.awangelo.msnucleo.model.Candidato
+import com.awangelo.msnucleo.repository.CandidatoRepository
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class CandidatoService {
+
+    private final CandidatoRepository candidatoRepository
+    private final PasswordEncoder passwordEncoder
+
+    CandidatoService(CandidatoRepository candidatoRepository, PasswordEncoder passwordEncoder) {
+        this.candidatoRepository = candidatoRepository
+        this.passwordEncoder = passwordEncoder
+    }
+
+    @Transactional
+    CandidatoResponseDTO salvar(CandidatoRequestDTO dto) {
+        String senha = passwordEncoder.encode(dto.senha)
+
+        Candidato candidato = new Candidato(
+                nome: dto.nome,
+                sobrenome: dto.sobrenome,
+                dataNascimento: dto.dataNascimento,
+                email: dto.email,
+                cpf: dto.cpf,
+                pais: dto.pais,
+                estado: dto.estado,
+                cep: dto.cep,
+                descricao: dto.descricao,
+                telefone: dto.telefone,
+                linkedin: dto.linkedin,
+                senha: senha
+        )
+
+        Candidato candidatoSalvo = candidatoRepository.save(candidato)
+
+        return new CandidatoResponseDTO(candidatoSalvo)
+    }
+
+}
