50ec50a8f934304e0456e6a687d0848c0ba02bd1
awangelo <anggg.gaio@gmail.com>
2026-03-18 10:20:23 -0300

    feat: create rest controller for Candidato creation

---
+++ ms-nucleo/src/main/groovy/com/awangelo/msnucleo/controller/CandidatoController.groovy
@@ -0,0 +1,35 @@
+package com.awangelo.msnucleo.controller
+
+import com.awangelo.msnucleo.dto.CandidatoRequestDTO
+import com.awangelo.msnucleo.dto.CandidatoResponseDTO
+import com.awangelo.msnucleo.service.CandidatoService
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/candidato")
+@CrossOrigin(origins = "*")
+class CandidatoController {
+
+    private final CandidatoService candidatoService
+
+    CandidatoController(CandidatoService candidatoService) {
+        this.candidatoService = candidatoService
+    }
+
+    @PostMapping
+    ResponseEntity<CandidatoResponseDTO> cadastrarCandidato(@Valid @RequestBody CandidatoRequestDTO requestDTO) {
+        // @Valid roda o Bean Validation ANTES de entrar no metodo.
+
+        CandidatoResponseDTO responseDTO = candidatoService.salvar(requestDTO)
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO)
+    }
+
+}
